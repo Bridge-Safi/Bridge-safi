e71ff64fd2dd4d64480ca0a8d631a54c0f3da24e
Bridge-Safi <khalidglioune@gmail.com>
2026-07-08 19:15:45 +0100

    Bump SW cache v12->v13 (Bridge Supermarché)

--- artifacts/grado-eats/public/sw.js
+++ artifacts/grado-eats/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bridge-safi-v12';
+const CACHE = 'bridge-safi-v13';
 const ASSETS = ['/', '/manifest.json', '/logo.jpeg', '/logo_splash.jpeg', '/logo_delivery.jpeg', '/logo_taxi.jpeg'];
 
 self.addEventListener('install', e => {
