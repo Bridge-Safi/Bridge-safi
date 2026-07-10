9eb6f1f4e2a9276150434f17371fe293acb62aff
Bridge-Safi <khalidglioune@gmail.com>
2026-07-10 13:51:32 +0100

    Bump cache SW v31 -> v32 (enseignes Boulangerie)

--- artifacts/grado-eats/public/sw.js
+++ artifacts/grado-eats/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bridge-safi-v31';
+const CACHE = 'bridge-safi-v32';
 const ASSETS = ['/', '/manifest.json', '/logo.jpeg', '/logo_splash.jpeg', '/logo_delivery.jpeg', '/logo_taxi.jpeg'];
 
 self.addEventListener('install', e => {
