b4c76c10f3559b1d341f4fb56dcc15aa9caa732a
Bridge-Safi <khalidglioune@gmail.com>
2026-07-10 21:39:47 +0100

    sw.js: bump cache v44->v45 (zone GPS elargie a tout Safi)

--- artifacts/grado-eats/public/sw.js
+++ artifacts/grado-eats/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bridge-safi-v44';
+const CACHE = 'bridge-safi-v45';
 const ASSETS = ['/', '/manifest.json', '/logo.jpeg', '/logo_splash.jpeg', '/logo_delivery.jpeg', '/logo_taxi.jpeg'];
 
 self.addEventListener('install', e => {
